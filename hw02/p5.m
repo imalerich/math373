36e8f8e19923b500a873b34211dabbf74efc44e7
Ian Malerich <ian.malerich@gmail.com>
2017-10-08 16:57:49 -0500

    99% sure this is wrong, but we'll go with it for now

---
+++ hw02/p5.m
@@ -0,0 +1,14 @@
+function p5
+
+x = [0 1 2];
+y = [0 1 2];
+
+%% Natural Cubic Spline
+
+[a b c d] = naturalcubicspline(x, y);
+
+%% Clamped Cubic Sline
+
+[a b c d] = clampedcubicspline(x,y,1,1)
+
+end
